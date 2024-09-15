d5b0006fbaa6a3b7970b3fc78da544d24b8c3ecf
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-15 13:50:32 +0530

    feat: progress bar value fixed to 100

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import ProgressBar from "./components/ProgressBar";
+import { useEffect } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    let interValTime = setInterval(() => {
+      setValue((prev) => prev + 1);
+    }, 100);
+
+    return () => {
+      clearInterval(interValTime);
+    };
+  }, []);
+
   return (
     <div className="progress-app">
       <h6>Progress Bar</h6>
-      <ProgressBar value={23} />
+      <ProgressBar value={value} />
     </div>
   );
 }
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
+
 const ProgressBar = ({ value = 0 }) => {
+  const [percent, setPercent] = useState(value);
+
+  useEffect(() => {
+    setPercent(Math.min(100, Math.max(value, 0)));
+  }, [value]);
+
   return (
     <div className="progress-bar">
-      <span>{value.toFixed()}%</span>
+      <span>{percent.toFixed()}%</span>
       <div></div>
     </div>
   );
